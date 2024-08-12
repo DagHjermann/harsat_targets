7fdfdab9c6a1b9f4f5736970387e15b0f346085c
Dag_Hjermann <dhj@niva.no>
2024-08-12 15:31:44 +0200

    _targets file:  init change branching approach

--- _targets.R
+++ _targets.R
@@ -7,6 +7,72 @@ tar_option_set(packages = c("harsat"))
 
 # Todo / improvements, see bottom:
 
+# Branching approach 1: 
+# Static branching, using 'targets_group' column in 'raw_data_sample.csv' 
+#   which then is preserved until branching actually is done, needing special functions 
+#   'create_timeseries_tar', 'output_timeseries_tar', 'tidy_data_tar' and 'tidy_contaminants_tar'
+
+# list(
+#   tar_target(file_info, "harsat_data/info.rds", format = "file"),
+#   tar_target(file_stations, "harsat_data/ICES_DOME_STATIONS_20230829_NO.csv", format = "file"),
+#   tar_target(file_contaminants, "harsat_data/raw_data_sample.csv", format = "file"),
+#   # tar_target(file_info, "raw_data_sample.csv", format = "file"),
+#   tar_target(
+#     biota_data,
+#     read_data_tar(
+#       compartment = "biota",
+#       purpose = "OSPAR",
+#       contaminants = file,   
+#       data_dir = file.path("harsat_data"),
+#       data_format = "external",
+#       info_dir = file.path("information", "OSPAR_2022"),
+#       filename_info = file_info,
+#       filename_stations = file_stations,
+#       filename_contaminants = file_contaminants
+#       )),
+#   tar_target(biota_data_tidy1, tidy_data_tar(biota_data)),
+#   tar_target(biota_data_tidy2, tidy_data2(biota_data_tidy1)),
+#   tar_target(
+#     biota_timeseries_all, 
+#     create_timeseries_tar(
+#       biota_data_tidy2,
+#       determinands = ctsm_get_determinands(biota_data_tidy2$info),
+#       determinands.control = NULL,
+#       oddity_path = oddities.dir,   # this doesn't seem to be respected, files are written to oddities/biota
+#       return_early = FALSE,
+#       print_code_warnings = FALSE,
+#       get_basis = get_basis_most_common,
+#       normalise = FALSE,
+#       normalise.control = list()
+#     )),
+#   tar_target(biota_timeseries_list, split_timeseries_object(biota_timeseries_all)),
+#   tar_target(info, biota_timeseries_all[["info"]]),
+#   tar_map(
+#     list(determinand = c("CD", "PFOS")),
+#     tar_target(biota_timeseries, biota_timeseries_list[[determinand]]),
+#     tar_target(
+#       biota_assessment,
+#       run_assessment_tar(
+#         biota_timeseries,
+#         info = info,
+#         # subset = sel_series,
+#         AC = NULL,
+#         get_AC_fn = NULL,
+#         recent_trend = 20,
+#         parallel = FALSE, 
+#         extra_data = NULL,
+#         control = list(power = list(target_power = 80, target_trend = 10)) 
+#       )),
+#     tar_target(
+#       biota_assess_data, get_assessment_data(biota_assessment))
+#     )
+# )
+
+# Branching approach 2: 
+# Static branching, using a separate file 'branching_groups.csv' at branching 
+#   - can then use the original harsat functions 'create_timeseries', 'output_timeseries', 
+#   'tidy_data' and 'tidy_contaminants'
+# Initial version: just Branching approach 1, copy-pasted
 
 list(
   tar_target(file_info, "harsat_data/info.rds", format = "file"),
@@ -25,7 +91,7 @@ list(
       filename_info = file_info,
       filename_stations = file_stations,
       filename_contaminants = file_contaminants
-      )),
+    )),
   tar_target(biota_data_tidy1, tidy_data_tar(biota_data)),
   tar_target(biota_data_tidy2, tidy_data2(biota_data_tidy1)),
   tar_target(
@@ -61,8 +127,9 @@ list(
       )),
     tar_target(
       biota_assess_data, get_assessment_data(biota_assessment))
-    )
+  )
 )
+
 
 # Improvement done:
 # Originally, 'biota_timeseries_list' and all 'biota_timeseries' contained each one copy of
